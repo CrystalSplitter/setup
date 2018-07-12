f7565e7121a0d0e7ae3517c7d647c45d46165294
Jordan R A <gamewhizzit@gmail.com>
2018-07-11 18:45:05 -0700

    Updated startup-install

--- startup-install.bash
+++ startup-install.bash
@@ -2,7 +2,7 @@
 
 # check if root first
 if [ "$EUID" -ne 0 ]; then
-    echo "Insufficient permissions. Are you running as root?"
+    echo 'Insufficient permissions. Are you running as root?'
     exit 1
 fi
 
@@ -14,25 +14,29 @@ rm '/tmp/sources.list'
 # ============================================================================
 
 apt install sudo
-printf "Running visudo... [press enter to continue]"
+printf 'Running visudo... [press enter to continue]'
 read tmp_
 visudo
 
-printf "Primary non-root username?> "
+printf 'Primary non-root username?> '
 read U
-printf "Reconfigure locale? [y/n]> "
+printf 'Reconfigure locale? [y/n]> '
 read LOCALE_CONFIG
-printf "Install Java-related packages? [y/n]> "
+printf 'Install Java-related packages? [y/n]> '
 read JAV_CONFIG
+printf 'Install desktop applications? [y/n]> '
+read DESKTOP_CONFIG
+
+echo 'BEGINNING INSTALL ==================================================='
 
 # Update the apt repository
 apt update
 apt upgrade
 
-apt install build-essential cmake firmware-linux-nonfree
+yes | apt install build-essential cmake firmware-linux-nonfree
 
-if [[ "$LOCALE_CONFIG" == "y" ]]; then
-    echo "Running dpkg-reconfigure to alter the locale..."
+if [[ "$LOCALE_CONFIG" == 'y' ]]; then
+    echo 'Running dpkg-reconfigure to alter the locale...'
     dpkg-reconfigure locales
 fi
 
@@ -41,20 +45,21 @@ apt install zsh
 ZSH_SUCC=$?
 if [[ "$ZSH_SUCC" == 0 ]]; then
     chsh -s /bin/zsh
-    runuser -l $U -c `chsh -s /bin/zsh`
+    runuser -l $U -c $(chsh -s /bin/zsh)
 fi
 
-apt install vim curl tmux
-apt install ffmpeg git python-pip python3-pip
+apt install -y vim curl tmux
+apt install -y ffmpeg git python-pip python3-pip fail2ban
 
-if [[ "$JAV_CONFIG" == "y" ]]; then
-    apt install openjdk-8-jre maven
+if [[ "$JAV_CONFIG" == 'y' ]]; then
+    yes | apt install openjdk-8-jre maven
 fi
 
-if [[ "$FAILBAN" == "y" ]]; then
-    apt install fail2ban
+if [[ "$DESKTOP_CONFIG" == 'y' ]]; then
+    echo 'Installing desktop applictaions... '
+    apt install -y audacity inkscape vlc texmaker krita steam snapd
 fi
 
-echo "!!! Make sure to edit your .zshrc and .vimrc !!!"
+echo '!!! Make sure to edit your .zshrc .vimrc .Xresources !!!'
 
 # Run the arbitrary installation files
