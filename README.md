d49f7ff2eb7832fa04417c426bdf9bb6689c0287
CrystalSplitter <gamewhizzit@gmail.com>
2021-06-12 16:37:29 -0700

    Add better configuration options, added grub conf

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# setup
+# Setup
 
 This repository contains a collection of setup scripts for rapid installation.
 
-At this time (2018-05-16), all scripts here are intended to run on Debian systems.
+~~At this time (2018-05-16), all scripts here are intended to run on Debian systems.~~
+
+# Grub fixes for AMD GPU & Gigabyte Motherboard
+
+[grub_config/default_grub] shows the grub configuration I use. This is normally located
+under `/etc/default/grub`.
--- ubuntu-2021-setup.bash
+++ ubuntu-2021-setup.bash
@@ -3,19 +3,25 @@
 set -e -x
 
 # check if root first
-if [ "$EUID" -ne 0 ]; then
+if [[ "$EUID" -ne 0 ]]; then
     echo 'Insufficient permissions. Are you running as root?'
     exit 1
 fi
 
-source "./ubuntu-2021-config.env"
+source './ubuntu-2021-config.env'
 
+function curl_deb {
+    url="$1"
+    curl -fLo '/tmp/new_pkg.deb' "$url"
+    apt install -y '/tmp/new_pkg.deb'
+    rm '/tmp/new_pkg.deb'
+}
 
 # ============================================================================
 
 add-apt-repository multiverse
 apt update
-apt upgrade
+apt upgrade -y
 
 apt install -y "${ALWAYS_INSTALL[@]}"
 
@@ -27,10 +33,22 @@ apt install -y "${ALWAYS_INSTALL[@]}"
 #    runuser -l $U -c $(chsh -s /bin/zsh)
 #fi
 
+user_shell='/bin/bash'
+
 # ---------------------------------------------------------------------
 
 if [[ ! -z "$INSTALL_GENERAL_DEV" ]]; then
     apt install -y "${GENERAL_DEV[@]}"
+
+    if [[ ! -z "$INSTALL_PLUG_NEOVIM" ]]; then
+        sudo -H -u "${_user_name}" \
+            sh -c \
+            'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    fi
+fi
+
+if [[ ! -z "$INSTALL_MESA_UTLS" ]]; then
+    apt install -y "${MESA_UTILS[@]}"
 fi
 
 if [[ ! -z "$INSTALL_CPP_CLANG_DEV" ]]; then
@@ -42,12 +60,20 @@ if [[ ! -z "$INSTALL_PYTHON_APT_DEV" ]]; then
 fi
 
 if [[ ! -z "$INSTALL_HASKELL_DEV" ]]; then
-    sudo -H -u "${_USER_NAME}" bash -c \
+    # These are necessary to install before ghcup.
+    apt install -y \
+        libffi-dev \
+        libffi7 \
+        libgmp10 \
+        libncurses-dev \
+        libncurses5 \
+        libtinfo5
+    sudo -H -u "${_user_name}" "${user_shell}" -c \
         'curl --proto "=https" --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh'
 fi
 
 if [[ ! -z "$INSTALL_RUST_DEV" ]]; then
-    sudo -H -u "${_USER_NAME}" bash -c \
+    sudo -H -u "${_user_name}" "${user_shell}" -c \
         'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh'
 fi
 
@@ -55,9 +81,7 @@ fi
 
 if [[ ! -z "$INSTALL_DESKTOP_APPS" ]]; then
     apt install -y "${DESKTOP_APPS[@]}"
-    curl -o '/tmp/discord.deb' -L 'https://discord.com/api/download?platform=linux&format=deb'
-    apt install -y '/tmp/discord.deb'
-    rm '/tmp/discord.deb'
+    curl_deb 'https://discord.com/api/download?platform=linux&format=deb'
     
     if [[ ! -z "$INSTALL_STEAM_APT" ]]; then
         apt install -y steam
@@ -68,6 +92,7 @@ if [[ ! -z "$INSTALL_DESKTOP_APPS" ]]; then
     fi
 
     if [[ ! -z "$INSTALL_CREATIVE_APPS" ]]; then
+        curl_deb 'https://github.com/DIGImend/digimend-kernel-drivers/releases/download/v10/digimend-dkms_10_all.deb'
         apt install -y "${CREATIVE_APPS[@]}"
     fi
 fi
